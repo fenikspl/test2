74d36a377b5eb4e61fb99295bdabf7d385ea315f
fenikspl <jaroslaw.warian@gmail.com>
2016-03-26 20:18:22 +0100

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-sudo apt-get install tightvncserver
+:program
+echo "Co wybierasz?"
+select x in install run quit
+do
+  case $x in
+    "install") sudo apt-get install tightvncserver ;;
+    "run" ) sudo tightvncserver ;;
+    "quit" ) echo "wyjscie" ;;
+    *) echo "jakis blad"
+  esac
+break
+done
